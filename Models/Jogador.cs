488cffefa27e72615a0f549518f7b1a2486a54d5
Gustavo <lima.gustavo100@hotmail.com>
2021-06-17 11:48:00 -0300

    Controller, View, Model

---
+++ Models/Jogador.cs
@@ -0,0 +1,12 @@
+namespace E_PlayersMVC.Models
+{
+    public class Jogador
+    {
+        public int IdJogador { get; set; }
+
+        public string NomeJogador { get; set; }
+        
+        public int IdEquipe { get; set; }
+
+    }
+}
